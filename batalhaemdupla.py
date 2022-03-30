21698b1f36821b5046c18a00f50bd2a0a70708ca
Gabriel <99111916+parreira7@users.noreply.github.com>
2022-03-30 15:30:14 -0300

    Update save as .png

--- batalhaemdupla.py
+++ batalhaemdupla.py
@@ -1,7 +1,7 @@
 import win32com.client
 import os
 psApp = win32com.client.Dispatch("Photoshop.Application")
-psApp.Open(r"C:\Users\Administrator\Desktop\Python\photoshoPY\thumnail_P.psd")
+psApp.Open(r"C:\Users\Administrator\Desktop\Python\photoshoPY\thumnail_P.psd") #(MUDAR NO SEU PC)
 doc = psApp.Application.ActiveDocument
 
 layerText = doc.ArtLayers["NOME1"] #selecionando a layer com o nome especifico
@@ -10,3 +10,8 @@ texto_layer = layerText.TextItem #listar como texto
 texto_layer1 = layerText1.TextItem #listar como texto
 texto_layer.contents = "mc X" #trocar o texto
 texto_layer1.contents = "mc Y" #trocar o texto
+save = win32com.client.Dispatch('Photoshop.ExportOptionsSaveForWeb') #selecionando a funçao de exportar 
+save.Format = 13 # formato que no caso, o png vale 13 na tabela
+save.PNG8 = False # seta como png 24-bit
+pngfile = (r"C:\Users\Administrator\Desktop\Python\photoshoPY\thumb.png") #lugar e nome onde a thumb irá ser salva (MUDAR NO SEU PC)
+doc.Export(ExportIn=pngfile, ExportAs=2, Options=save) #chamando a funçao com tudo já setado (path, formato etc)
