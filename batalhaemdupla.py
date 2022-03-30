420e8392e46d51c5539b69190b55cd93d59fa1f5
Gabriel <99111916+parreira7@users.noreply.github.com>
2022-03-30 15:47:08 -0300

    Update batalhaemdupla.py

--- batalhaemdupla.py
+++ batalhaemdupla.py
@@ -1,17 +1,31 @@
+#{} []
 import win32com.client
 import os
+from os.path import exists
 psApp = win32com.client.Dispatch("Photoshop.Application")
-psApp.Open(r"C:\Users\Administrator\Desktop\Python\photoshoPY\thumnail_P.psd") #(MUDAR NO SEU PC)
+psApp.Open(r"C:\Users\Administrator\Desktop\Python\photoshoPY\thumnail_P.psd")
 doc = psApp.Application.ActiveDocument
 
 layerText = doc.ArtLayers["NOME1"] #selecionando a layer com o nome especifico
 layerText1 = doc.ArtLayers["NOME2"] #selecionando a layer com o nome especifico
+layerText2 = doc.ArtLayers["NOME3"] #selecionando a layer com o nome especifico
+layerText2.visible = False # esconder pq é em dupla e nao precisa de 3 nomes
+
 texto_layer = layerText.TextItem #listar como texto
 texto_layer1 = layerText1.TextItem #listar como texto
-texto_layer.contents = "mc X" #trocar o texto
-texto_layer1.contents = "mc Y" #trocar o texto
+
+texto_layer.contents = "abcd" #trocar o texto (AQUI ONDE A MAGICA ACONTECE BICHO)
+texto_layer1.contents = "abcd 1" #trocar o texto (AQUI ONDE A MAGICA ACONTECE BICHO)
+
 save = win32com.client.Dispatch('Photoshop.ExportOptionsSaveForWeb') #selecionando a funçao de exportar 
 save.Format = 13 # formato que no caso, o png vale 13 na tabela
 save.PNG8 = False # seta como png 24-bit
-pngfile = (r"C:\Users\Administrator\Desktop\Python\photoshoPY\thumb.png") #lugar e nome onde a thumb irá ser salva (MUDAR NO SEU PC)
+pngfile = (r"C:\Users\Administrator\Desktop\Python\photoshoPY\thumb.png") #lugar e nome onde a thumb irá ser salva
 doc.Export(ExportIn=pngfile, ExportAs=2, Options=save) #chamando a funçao com tudo já setado (path, formato etc)
+
+#checar se o arquivo existe e fechar o programa
+arquivo_existe = os.path.exists(r'C:\Users\Administrator\Desktop\Python\photoshoPY\thumb.png') #mudar no seu pc
+print(arquivo_existe)#printa se existe ou nao
+if (arquivo_existe == True): #se existir vai fechar
+    doc = psApp.Application.ActiveDocument.Close(1) #salvar e fechar o .psd
+    psApp.Quit() #fechar o photoshop
